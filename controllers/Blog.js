ffdb434c2bd3c061e1ddd5e8f6e56a2df5923b4c
Ademola <ademolaogunmokun@yahoo.com>
2022-06-25 19:31:56 +0100

    task

---
+++ controllers/Blog.js
@@ -0,0 +1,15 @@
+const blogRouter = require('express').Router();
+const Blog = require("../models/Blog");
+
+blogRouter.get('/', (req, res) => {
+  Blog.find({}).then(blogs => res.json(blogs))
+});
+
+blogRouter.post('/', (req, res, next ) => {
+  const newBlog = new Blog(req.body)
+  newBlog.save().then(savedBlog => res.json(savedBlog)).catch(error => {
+    next(error)
+  })
+});
+
+module.exports = blogRouter
