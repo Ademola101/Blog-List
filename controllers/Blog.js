454f4cb04ddd873e49835fcd3196bdd74eb986eb
Ademola <ademolaogunmokun@yahoo.com>
2022-06-26 17:06:16 +0100

    lint

--- controllers/Blog.js
+++ controllers/Blog.js
@@ -1,15 +1,15 @@
 const blogRouter = require('express').Router();
-const Blog = require("../models/Blog");
+const Blog = require('../models/Blog');
 
 blogRouter.get('/', (req, res) => {
-  Blog.find({}).then(blogs => res.json(blogs))
+  Blog.find({}).then((blogs) => res.json(blogs));
 });
 
-blogRouter.post('/', (req, res, next ) => {
-  const newBlog = new Blog(req.body)
-  newBlog.save().then(savedBlog => res.json(savedBlog)).catch(error => {
-    next(error)
-  })
+blogRouter.post('/', (req, res, next) => {
+  const newBlog = new Blog(req.body);
+  newBlog.save().then((savedBlog) => res.json(savedBlog)).catch((error) => {
+    next(error);
+  });
 });
 
-module.exports = blogRouter
+module.exports = blogRouter;
