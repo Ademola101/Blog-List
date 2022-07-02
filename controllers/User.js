53cdcef850da7f42ae96e5b048ea6827b187a654
Ademola <ademolaogunmokun@yahoo.com>
2022-07-02 21:41:36 +0100

    attempt creation user

--- controllers/User.js
+++ controllers/User.js
@@ -1,7 +1,24 @@
 const userRouter = require('express').Router();
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 userRouter.get('/', async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
+
+userRouter.post('/', async (req, res) => {
+  const { username, name, password } = req.body;
+  const saltround = 10;
+  const passwordHash = await bcrypt.hash(password, saltround);
+  const newUser = User.new({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const saveUser = await newUser.save();
+  res.status(201).json(saveUser);
+});
+
+module.exports = userRouter;
